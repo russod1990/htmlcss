50cd4aa50ffead85eeaaa1bb77f8dfc03bfa67d5
tss <tss@debian-tss24>
2017-03-14 11:02:32 +0100

    HTML5

--- public_html/lez8/stati/jsStati.js
+++ public_html/lez8/stati/jsStati.js
@@ -9,20 +9,61 @@ var direzione = 1;
 var aX = 0;
 var aY = 0;
 var contatore = 0;
+var idTimer = 0;
 
 $("#go").click(function () {
-    setInterval(function () {
+    $("#go").attr("diabled", "disabled")
+    document.getElementById("go").disabled;
+    idTimer = setInterval(function () {
         if (direzione === 1) {
-            if (contatore < 12) {
+            if (contatore < 19) {
                 aX += 60;
+                $("div #gif").css(
+                        "left", aX);
                 contatore++;
-                $("div #gif").css({
-                    "left": aX
-                });
+                ;
             } else {
-                direzione=2;
+                direzione = 2;
             }
         }
-        
-    });
-}, 30);
+        if (direzione === 2) {
+            if (contatore < 28) {
+                aY += 60;
+                $("div #gif").css(
+                        "top", aY);
+                contatore++;
+                ;
+            } else {
+                direzione = 3;
+            }
+        }
+        if (direzione === 3) {
+            if (contatore < 47) {
+                aX -= 60;
+                $("div #gif").css(
+                        "left", aX);
+                contatore++;
+                ;
+            } else {
+                direzione = 4;
+            }
+        }
+        if (direzione === 4) {
+            if (contatore < 56) {
+                aY -= 60;
+                $("div #gif").css(
+                        "top", aY);
+                contatore++;
+                ;
+            } else {
+                direzione = 1;
+                contatore = 0;
+            }
+        }
+    }, 30);
+});
+$("#pStop").click(function () {
+    clearInterval(idTimer);
+    $("#go").removeAttr("disabled");
+    document.getElementById("go").enabled;
+});
