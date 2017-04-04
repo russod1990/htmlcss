1972ed2522ed1e3050cd4f030d7aeedfc8e04c5f
tss <tss@debian-tss24>
2017-04-04 16:31:39 +0200

    HTML5

---
+++ public_html/lez11/folder/javascript.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$("#save").click(function() {
+    var obj={};
+    obj.testo=$("#testo").val();
+    console.log(obj);
+});
